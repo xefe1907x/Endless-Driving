ade2edb4be851b7c95b47d936d9ffba64a5705ae
xefe1907x <efeaslan1995@hotmail.com>
2022-11-17 13:57:45 +0300

    Car moves forward with acceleration and car can go sides if we touch and swipe

--- Assets/Scripts/Car/CarMovement.cs
+++ Assets/Scripts/Car/CarMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TouchPhase = UnityEngine.TouchPhase;
 
 public class CarMovement : MonoBehaviour
 {
@@ -7,18 +8,51 @@ public class CarMovement : MonoBehaviour
     [SerializeField] float carAcceleration = 0.2f;
     [Space(5)]
     
-    [Header("Steer")]
-    [SerializeField] float steerValue = 10f;
+    [Header("MoveSides")]
+    [SerializeField] [Range(0.001f, 0.1f)] float sideSpeed;
+
+    Vector2 firstPosition;
+    Vector2 currentPosition;
+
     void Update()
     {
         MoveForward();
+        MoveSides();
         IncreaseSpeedRegularly();
-        SteerCar();
+        GettingTouchInputs();
     }
 
-    void SteerCar()
+    void MoveSides()
     {
-        // transform.Rotate(0f, );
+        var deltaPositionX = currentPosition.x - firstPosition.x;
+        
+        if (Input.touchCount > 0)
+        {
+            transform.position = new Vector3(transform.position.x + deltaPositionX * sideSpeed * Time.deltaTime,
+                transform.position.y, transform.position.z);
+        }
+    }
+
+    void GettingTouchInputs()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                firstPosition = Input.GetTouch(0).position;
+            }
+
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                currentPosition = Input.GetTouch(0).position;
+            }
+
+            if (Input.GetTouch(0).phase == TouchPhase.Canceled)
+            {
+                firstPosition = Vector2.zero;
+                currentPosition = Vector2.zero;
+            }
+        }
     }
 
     void IncreaseSpeedRegularly()
@@ -29,10 +63,5 @@ public class CarMovement : MonoBehaviour
     void MoveForward()
     {
         transform.Translate(Vector3.forward * carSpeed * Time.deltaTime);
-    }
-
-    public void Steer(int value)
-    {
-        steerValue = value;
     }
 }
